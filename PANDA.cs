4b7b10cf036e3e36bf7259c2b0959ad0afc2eb8b
ManzanoFernanda <65313674+ManzanoFernanda@users.noreply.github.com>
2020-10-25 22:32:12 -0300

    Add files via upload

---
+++ PANDA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+using CHOSE;
+
+
+namespace CHOSE
+{
+    public partial class PANDA : Form
+    {
+        public PANDA()
+        {
+            InitializeComponent();
+        }
+
+        private void PANDA_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ POLAR.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+using CHOSE;
+
+
+namespace CHOSE
+{
+    public partial class POLAR : Form
+    {
+        public POLAR()
+        {
+            InitializeComponent();
+        }
+
+        private void POLAR_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
